61fe96bedb56d08281d06bc21b87f9b4024487c8
Iorga <iorgaflorinalexandru@gmail.com>
2023-02-16 00:02:31 +0200

    Empty interfaces

--- Infrastructure/ConfigureInfrastructure.cs
+++ Infrastructure/ConfigureInfrastructure.cs
@@ -1,3 +1,5 @@
+using Domain.Repositories;
+using Infrastructure.Repository;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -7,6 +9,8 @@ public static class ConfigureInfrastructure
 {
     public static IServiceCollection ConfigureInfrastructureServices(this IServiceCollection services, IConfiguration config)
     {
+        services.AddScoped<IRepositoryWrapper,RepositoryWrapper>();
+
         return services;
     }
 }
